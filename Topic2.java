2f8daba09925d47feaebedddab4f4506e8f02441
sakshamkulshrestha <25etim44@uohyd.ac.in>
2026-08-04 20:59:57 +0530

    Add AtmosphericSensor class and update Topic2 to display concentrationPPM

---
+++ Topic2.java
@@ -0,0 +1,28 @@
+class AtmosphericSensor{
+    String targetGas;
+    double concentrationPPM;
+    boolean isCalibrated = false;
+    
+    public String getTargetGas() {
+        return targetGas;
+    }
+    public double getConcentrationPPM() {
+        return concentrationPPM;
+    }
+    public boolean isCalibrated() {
+        return isCalibrated;
+    }
+
+    
+}
+public class Topic2 {
+    public static void main(String[] args) {
+        AtmosphericSensor obj = new AtmosphericSensor();
+        obj.targetGas = "Methane";
+        obj.concentrationPPM = 405.2;
+
+        System.out.println(obj.getTargetGas());
+        System.out.println(obj.getConcentrationPPM());
+
+    }
+}
