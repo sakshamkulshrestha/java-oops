8cd87e11d676be8943251ccafabe94714cc18e57
sakshamkulshrestha <25etim44@uohyd.ac.in>
2026-08-04 21:06:24 +0530

    Add NavigationGyroscope class and implement applyTorque method in Topic3

---
+++ Topic3.java
@@ -0,0 +1,14 @@
+class NavigationGyroscope{
+    double zAxisSpinRate = 0.0;
+    
+    public void applyTorque(double force){
+        zAxisSpinRate += force * 1.5;
+        System.out.println(zAxisSpinRate);
+    }
+}
+public class Topic3 {
+    public static void main(String[] args) {
+        NavigationGyroscope obj = new NavigationGyroscope();
+        obj.applyTorque(10);
+    }
+}
