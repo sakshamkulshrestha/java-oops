6915ee9e246b8ba3b73f81572bb5267b62946510
sakshamkulshrestha <25etim44@uohyd.ac.in>
2026-08-05 20:52:43 +0530

    Add CultureIncubation class to initiate bacterial culture inoculation with current date

---
+++ CultureIncubation.java
@@ -0,0 +1,8 @@
+import java.time.LocalDate;
+
+public class CultureIncubation {
+    public static void main(String[] args) {
+        LocalDate inoculationDate = LocalDate.now();
+        System.out.println("Bacterial culture inoculation initiated on: " + inoculationDate);
+    }
+}
