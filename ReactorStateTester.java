2fafa5b61f1de14bd68e2b8b6530d2a5f79c87c8
sakshamkulshrestha <25etim44@uohyd.ac.in>
2026-08-05 20:45:02 +0530

    Add ReactorStateTester class with ReactorStatus enum and emergency message handling

---
+++ ReactorStateTester.java
@@ -0,0 +1,16 @@
+enum ReactorStatus{
+    COLD_STANDBY,
+    POWER_ASCENSION,
+    CRITICAL,
+    SCRAM_TRIGGERED
+}
+
+public class ReactorStateTester {
+    public static void main(String[] args) {
+        ReactorStatus obj = ReactorStatus.SCRAM_TRIGGERED;
+        
+        if(obj == ReactorStatus.SCRAM_TRIGGERED){
+            System.out.println("EMERGENCY: Immediate coolant injection required!");
+        }
+    }
+}
