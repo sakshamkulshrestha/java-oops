95819010d1ecedf375e727122bd2f8b5cadec6e4
sakshamkulshrestha <25etim44@uohyd.ac.in>
2026-08-10 05:47:40 +0530

    refactor: enhance ErrorFixer class with improved error handling for division by zero

---
+++ VerdictRegistry.java
@@ -0,0 +1,9 @@
+public class VerdictRegistry {
+    public static void main(String[] args) {
+        int closedCaseYears = 5;
+        Integer pendingCaseYears = null; // we cannot use primitive type, because it doesnt allow us to set value to null
+
+        System.out.println(closedCaseYears);
+        System.out.println(pendingCaseYears);
+    }
+}
